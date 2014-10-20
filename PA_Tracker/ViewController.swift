9c9ef0875f8a9b4ddfdbb5a31539c25b4769a777
trey92 <treyaucoin@treys-mbp.usla.ibm.com>
2014-10-20 11:49:50 -0500

    Initial commit from local git

---
+++ PA_Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PA_Tracker
+//
+//  Created by Trey Aucoin on 10/20/14.
+//  Copyright (c) 2014 IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
